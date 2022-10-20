ab8910d38a0ce616e1028c887208811a0e4e4844
DivyPagariya22 <divypagariya11@gmail.com>
2022-10-20 20:07:21 +0530

    security update

--- next.config.js
+++ next.config.js
@@ -6,8 +6,24 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+];
+
 module.exports = {
   images: {
     domains: ["images.unsplash.com"],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
